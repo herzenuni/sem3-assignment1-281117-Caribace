1631c938888d7f13f2564d50d8c7e98b2b1c43e9
Mikhail Nikanorov <31664472+Caribace@users.noreply.github.com>
2018-01-15 22:53:04 +0300

    Create testtask2.py

---
+++ testtask2.py
@@ -0,0 +1,17 @@
+def function(number):
+  num=str(number)
+  sum=0
+  for el in num:
+      elem=int(el)
+      sum+=elem**2
+  return sum 
+
+def func():
+  for i in range(10, 100):
+    if function(i) % 17 == 0:
+      print('Двузначное число: {}'.format(i))  
+  for i in range(100, 1000):
+    if function(i) % 17 == 0:
+      print('Трехзначное число: {}'.format(i))
+      
+      func()     
